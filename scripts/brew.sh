6a48f9747ae4a2b9539f591c832dfa977191bdbf
Vaios Karampinis <oden@oden.gr>
2019-03-27 22:41:24 +0200

    rename homebrew

---
+++ scripts/brew.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# Homebrew
+#
+# This installs some of the common dependencies needed (or at least desired)
+# using Homebrew.
+
+# Check for Homebrew
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+exit 0
