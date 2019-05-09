ca79ff0600b42a696e5bdf86836abf45b989afb8
Vaios Karampinis <oden@oden.gr>
2019-05-09 22:02:26 +0300

    install apps from app store

---
+++ scripts/apps.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+TOOLS="_apps.txt"
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+. scripts/_utils.sh
+
+echo "Check apps to install..."
+
+sed 's/[[:space:]]*#.*//;/^[[:space:]]*$/d' $SCRIPTPATH/$TOOLS | while read app; do
+  KEY="${app%%::*}"
+  VALUE="${app##*::}"
+
+  if ! mas list | grep $KEY &> /dev/null; then
+    echo "Install $VALUE"
+    mas install $KEY
+  fi
+done
