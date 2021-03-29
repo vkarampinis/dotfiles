1b8681589fa2bcccb79bf10efdfb1a0c0fc4e2fb
Vaios Karampinis <oden@oden.gr>
2021-03-29 21:33:40 +0300

    add Calendar to cmd+6

--- config/.hammerspoon/init.lua
+++ config/.hammerspoon/init.lua
@@ -7,4 +7,5 @@ hs.hotkey.bind({"⌘"}, "2", function() toggleApplication("Visual Studio Code") 
 hs.hotkey.bind({"⌘"}, "3", function() toggleApplication("Slack") end)
 hs.hotkey.bind({"⌘"}, "4", function() toggleApplication("Mailspring") end)
 hs.hotkey.bind({"⌘"}, "5", function() toggleApplication("iTerm") end)
+hs.hotkey.bind({"⌘"}, "6", function() toggleApplication("Calendar") end)
 
