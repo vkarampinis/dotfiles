4cf1e5188c2f916f2b4323ef86659fbd93ba981e
Vaios Karampinis <oden@oden.gr>
2021-03-31 17:52:25 +0300

    Revert "set safari as default browser"
    This reverts commit 71d91c4d371ac4f6cd9ec527aded0d1eb32195f6.

--- config/.hammerspoon/init.lua
+++ config/.hammerspoon/init.lua
@@ -2,7 +2,7 @@ local function toggleApplication(name)
   hs.application.launchOrFocus(name)
 end
 
-hs.hotkey.bind({"⌘"}, "1", function() toggleApplication("Safari") end)
+hs.hotkey.bind({"⌘"}, "1", function() toggleApplication("Brave Browser") end)
 hs.hotkey.bind({"⌘"}, "2", function() toggleApplication("Visual Studio Code") end)
 hs.hotkey.bind({"⌘"}, "3", function() toggleApplication("Slack") end)
 hs.hotkey.bind({"⌘"}, "4", function() toggleApplication("Mailspring") end)
