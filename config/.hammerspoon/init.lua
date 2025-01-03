f6d9d2df9f009c038a07e076d467f916f1d7b128
Vaios Karampinis <oden@oden.gr>
2025-01-03 20:56:00 +0200

    refactor(hammerspoon): update hotkey bindings for applications

--- config/.hammerspoon/init.lua
+++ config/.hammerspoon/init.lua
@@ -55,9 +55,9 @@ end
 
 
 hs.hotkey.bind({"⌘"}, "1", function() toggleApplication("Brave Browser") end)
-hs.hotkey.bind({"⌘"}, "2", function() toggleApplication("Visual Studio Code") end)
--- hs.hotkey.bind({"⌘"}, "3", function() toggleApplication("Microsoft Teams") end)
-hs.hotkey.bind({"⌘"}, "3", function() toggleApplication("Microsoft Teams (work preview)") end)
+-- hs.hotkey.bind({"⌘"}, "2", function() toggleApplication("Visual Studio Code") end)
+hs.hotkey.bind({"⌘"}, "2", function() toggleApplication("Cursor") end)
+hs.hotkey.bind({"⌘"}, "3", function() toggleApplication("Microsoft Teams") end)
 hs.hotkey.bind({"⌘"}, "4", function() toggleApplication("Mail") end)
 hs.hotkey.bind({"⌘"}, "5", function() toggleApplication("iTerm") end)
 hs.hotkey.bind({"⌘"}, "6", function() toggleApplication("Calendar") end)
